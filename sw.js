f91c336b6016aff5aff306f9614eecf35343e909
vseriousv <gavrilovskirill@gmail.com>
2019-12-12 16:46:08 +0700

    first commit

---
+++ sw.js
@@ -0,0 +1,6 @@
+'use strict';
+importScripts('sw-toolbox.js');
+
+toolbox.precache(['index.html', 'style/style.css']);
+toolbox.router.get('/img/*', toolbox.cacheFirst);
+toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 5});
