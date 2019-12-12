c2b8e20c0141a90720c7c357b9f233fa23138a02
vseriousv <gavrilovskirill@gmail.com>
2019-12-12 22:04:13 +0700

    new pwa portfolio from react

---
+++ precache-manifest.555247eae33715a2bd6bd6656a07f3fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7660a576b0a0b8ff89c535acb9718095",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31c0469aa72fa8d8f8a1",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1fd2f3c1a33236736ff7",
+    "url": "/static/js/2.e46c85f3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.e46c85f3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "31c0469aa72fa8d8f8a1",
+    "url": "/static/js/main.bf35f16a.chunk.js"
+  },
+  {
+    "revision": "96c665538e8d3a75dfd7",
+    "url": "/static/js/runtime-main.0abaa489.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
